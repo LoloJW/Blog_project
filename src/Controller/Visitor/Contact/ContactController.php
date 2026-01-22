5ee49c48d55d88a091fea5fb5ca1acb780b2b633
LoloJW <laurentsaint-georges@live.fr>
2026-01-22 11:10:16 +0100

    Ajout des pages de la section visiteur

--- src/Controller/Visitor/Contact/ContactController.php
+++ src/Controller/Visitor/Contact/ContactController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_visitor_contact_index', methods:['GET'])]
+    #[Route('/contact', name: 'app_visitor_contact_index', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('pages/visitor/contact/index.html.twig');
--- src/Controller/Visitor/Welcome/WelcomeController.php
+++ src/Controller/Visitor/Welcome/WelcomeController.php
@@ -8,14 +8,15 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class WelcomeController extends AbstractController
 {
-    #[Route('/', name: 'app_visitor_welcome', methods:['GET'])]
+    #[Route('/', name: 'app_visitor_welcome', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('pages/visitor/welcome/index.html.twig');
     }
-    #[Route('/liste-des-articles',name: 'app_visitor_blog_index', methods:['GET'])]
+
+    #[Route('/liste-des-articles', name: 'app_visitor_blog_index', methods: ['GET'])]
     public function blog(): Response
     {
-     return $this->render('pages/visitor/blog/index.html.twig');  
+        return $this->render('pages/visitor/blog/index.html.twig');
     }
 }
