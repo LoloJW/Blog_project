e5bb9e8134a44e7f9f48d6ae859f4c88c8c9a7f9
LoloJW <laurentsaint-georges@live.fr>
2026-02-02 09:36:46 +0100

    Explication fuseau horaire

--- src/Trait/TimeZoneTrait.php
+++ src/Trait/TimeZoneTrait.php
@@ -3,7 +3,13 @@
 namespace App\Trait;
 
 trait TimeZoneTrait
-{
+{   
+    /**
+     * Permet de changer le fuseau horaire de l'application
+     *
+     * @param string $timeZoneId
+     * @return void
+     */
     protected function changeTimeZone(string $timeZoneId): void
     {
         date_default_timezone_set($timeZoneId);
